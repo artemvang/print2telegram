a6118557ff86d50c7c1aaa8418acccb5233b586d
vang_machine <vangogius@gmail.com>
2018-04-21 15:12:26 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="p2tg",
-      version="0.1.6",
+      version="0.2",
       description="Redirects stdout stream contents to telegram chat",
       license="Apache License 2.0",
       author="Artem Vang",
